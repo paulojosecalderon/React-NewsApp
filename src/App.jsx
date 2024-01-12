7c3aba11560f5a1c1ac789cd11274cbd4ec57c82
Paulo Calderon <paulojosecalderon@gmail.com>
2024-01-12 13:09:23 +0800

    Setup of React and BootStrap, components and Navbar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import Navbar from './components/Navbar'
+
+function App() {
+  
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
